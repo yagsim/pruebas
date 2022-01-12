6b1a577ab7fe30a89679c6d349ec738233e5eceb
Roi Martinez <rmartinezroque@danielcastelao.org>
2022-01-12 14:10:14 +0100

    añadida clase Funciones

---
+++ src/Funciones.java
@@ -0,0 +1,10 @@
+public class Funciones {
+    private final int CONST = 3;
+    private String nombre;
+
+    public Funciones() {
+    }
+    public Funciones(String nombre) {
+        this.nombre = nombre;
+    }
+}
